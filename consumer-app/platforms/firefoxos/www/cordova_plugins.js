08b82cce380ba120774afdf35c5c12cc727aa86f
svaithiyanathan <svaithiyanathan@paypal.com>
2015-08-12 10:43:58 +0530

    Added helloworld phonegap application, can be used as skeleton for developing phonegap consumer app

---
+++ consumer-app/platforms/firefoxos/www/cordova_plugins.js
@@ -0,0 +1,51 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/src/firefoxos/notification.js",
+        "id": "cordova-plugin-dialogs.dialogs-impl",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/firefoxos/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-console": "1.0.1",
+    "cordova-plugin-dialogs": "1.1.1",
+    "cordova-plugin-device": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
